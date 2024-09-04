63f5719da44b88112c010121f08f922823ea1c99
amirulal044 <139250166+amirulal044@users.noreply.github.com>
2024-09-04 23:57:22 +0700

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -84,6 +84,8 @@ function hidePantai(divId) {
 }
 
 
+
+
 function showMakam(divId,  buttonId) {
     // Menyembunyikan semua elemen dengan class 'content-makam'
     const makamDivs = document.querySelectorAll('.content-makam');
@@ -118,6 +120,42 @@ function hideMakam(divId) {
       buttons.forEach(button => button.classList.remove('active'));
 }
 
+
+
+
+function showEdukasi(divId,  buttonId) {
+    // Menyembunyikan semua elemen dengan class 'content-edukasi'
+    const edukasiDivs = document.querySelectorAll('.content-edukasi');
+    edukasiDivs.forEach(div => div.style.display = 'none');
+    
+    // Menampilkan elemen yang dipilih
+    const divToShow = document.getElementById(divId);
+    if (divToShow) {
+        divToShow.style.display = 'block';
+    }
+
+     // Menghapus kelas 'active' dari semua tombol
+     const buttons = document.querySelectorAll('.button');
+     buttons.forEach(button => button.classList.remove('active'));
+ 
+     // Menambahkan kelas 'active' pada tombol yang diklik
+     const activeButton = document.getElementById(buttonId);
+     if (activeButton) {
+         activeButton.classList.add('active');
+     }
+}
+
+function hideEdukasi(divId) {
+    // Menyembunyikan elemen yang dipilih
+    const divToHide = document.getElementById(divId);
+    if (divToHide) {
+        divToHide.style.display = 'none';
+    }
+
+      // Menghapus kelas 'active' dari semua tombol
+      const buttons = document.querySelectorAll('.button');
+      buttons.forEach(button => button.classList.remove('active'));
+}
 
 
 
@@ -190,4 +228,278 @@ document.addEventListener("DOMContentLoaded", function() {
     shuffleArray(images);
 
     images.forEach(image => gallery.appendChild(image));
+});
+
+
+
+
+
+/*AKTIVITAS*/
+document.addEventListener('DOMContentLoaded', () => {
+    const activitiessemat = {
+        berenang: {
+            image: 'wisata-alam/pantai-1.jpg',
+            title: 'Berenang',
+            description: 'Nikmati Kegembiraan Berenang: Pantai Semat menawarkan air laut yang jernih dan menyegarkan, sempurna untuk berenang dan bermain air bersama keluarga dan teman-teman. Rasakan ketenangan dan kebahagiaan saat Anda berenang di ombak yang tenang dan bersih.'
+        },
+        ayunan: {
+            image: 'wisata-alam/pantai-1.jpg',
+            title: 'Ayunan',
+            description: 'Ayunan di Tepi Pantai: Pantai Semat menawarkan pengalaman unik bermain ayunan dengan pemandangan laut yang memukau. Nikmati hembusan angin laut yang menyegarkan sambil berayun-ayun di bawah pohon kelapa yang rindang. Ini adalah cara sempurna untuk bersantai dan menikmati alam.'
+        },
+        jalan: {
+            image: 'wisata-alam/pantai-1.jpg',
+            title: 'Jalan Santai',
+            description: 'Jalan Santai di Tepi Pantai: Pantai Semat menawarkan pengalaman yang tenang dan menyegarkan dengan berjalan kaki di sepanjang garis pantai. Nikmati pasir putih yang lembut di bawah kaki Anda dan deburan ombak yang menenangkan. Jalan santai di pantai adalah cara yang ideal untuk merelaksasikan pikiran dan tubuh Anda.'
+        },
+        sepeda: {
+            image: 'wisata-alam/pantai-1.jpg',
+            title: 'Sepeda',
+            description: 'Jalan Santai di Tepi Pantai: Pantai Semat menawarkan pengalaman yang tenang dan menyegarkan dengan berjalan kaki di sepanjang garis pantai. Nikmati pasir putih yang lembut di bawah kaki Anda dan deburan ombak yang menenangkan. Jalan santai di pantai adalah cara yang ideal untuk merelaksasikan pikiran dan tubuh Anda.'
+        },
+        mancing: {
+            image: 'wisata-alam/pantai-1.jpg',
+            title: 'Mancing',
+            description: ' Spot Memancing Terbaik: Pantai Semat menawarkan spot-spot memancing yang strategis dan kaya ikan. Dari bibir pantai yang landai hingga area perairan yang lebih dalam, Anda akan menemukan berbagai jenis ikan yang siap menyambut umpan Anda. Nikmati kesempatan untuk menangkap ikan-ikan lokal yang lezat dan berharga sebagai hasil dari usaha Anda.'
+        },
+        spotfoto: {
+            image: 'wisata-alam/pantai-1.jpg',
+            title: 'Spot Foto',
+            description: 'Pemandangan Laut yang Menakjubkan: Pantai Semat menawarkan pemandangan laut yang spektakuler, ideal untuk foto-foto yang memukau. Dari cakrawala yang membentang luas hingga warna-warni matahari terbenam, setiap sudut pantai ini memberikan latar belakang yang sempurna untuk foto-foto indah. Ambil gambar saat matahari terbenam atau matahari terbit untuk hasil yang menakjubkan dan penuh warna.'
+        }
+    };
+
+    const icons = document.querySelectorAll('.icon');
+    const activityImage = document.getElementById('activity-image-pantaisemat');
+    const activityTitle = document.getElementById('activity-title-pantaisemat');
+    const activityDescription = document.getElementById('activity-description-pantaisemat');
+    let activeIcon = null;
+
+    function updateActivity(activity) {
+        activityImage.src = activitiessemat[activity].image;
+        activityTitle.textContent = activitiessemat[activity].title;
+        activityDescription.textContent = activitiessemat[activity].description;
+
+        icons.forEach(i => {
+            i.classList.remove('active-pantaisemat');
+        });
+        activeIcon = document.querySelector(`[data-activity-pantaisemat="${activity}"]`);
+        activeIcon.classList.add('active-pantaisemat');
+    }
+
+    icons.forEach(icon => {
+        icon.addEventListener('click', () => {
+            const activity = icon.getAttribute('data-activity-pantaisemat');  
+            updateActivity(activity);
+        });
+    });
+
+    // Set default activity
+    const defaultActivity = 'berenang';
+    updateActivity(defaultActivity);
+});
+
+
+
+/*AKTIVITAS*/
+document.addEventListener('DOMContentLoaded', () => {
+    const activitiesmbahsirah = {
+        berenang: {
+            image: 'wisata-alam/pantai-1.jpg',
+            title: 'Berenang',
+            description: 'Nikmati Kegembiraan Berenang: Pantai Mbah Sirah menawarkan air laut yang jernih dan menyegarkan, sempurna untuk berenang dan bermain air bersama keluarga dan teman-teman. Rasakan ketenangan dan kebahagiaan saat Anda berenang di ombak yang tenang dan bersih.'
+        },
+        ayunan: {
+            image: 'wisata-alam/pantai-1.jpg',
+            title: 'Ayunan',
+            description: 'Ayunan di Tepi Pantai: Pantai Mbah Sirah menawarkan pengalaman unik bermain ayunan dengan pemandangan laut yang memukau. Nikmati hembusan angin laut yang menyegarkan sambil berayun-ayun di bawah pohon kelapa yang rindang. Ini adalah cara sempurna untuk bersantai dan menikmati alam.'
+        },
+        jalan: {
+            image: 'wisata-alam/pantai-1.jpg',
+            title: 'Jalan Santai',
+            description: 'Jalan Santai di Tepi Pantai: Pantai Mbah Sirah menawarkan pengalaman yang tenang dan menyegarkan dengan berjalan kaki di sepanjang garis pantai. Nikmati pasir putih yang lembut di bawah kaki Anda dan deburan ombak yang menenangkan. Jalan santai di pantai adalah cara yang ideal untuk merelaksasikan pikiran dan tubuh Anda.'
+        },
+        sepeda: {
+            image: 'wisata-alam/pantai-1.jpg',
+            title: 'Sepeda',
+            description: 'Jalan Santai di Tepi Pantai: Pantai Mbah Sirah menawarkan pengalaman yang tenang dan menyegarkan dengan berjalan kaki di sepanjang garis pantai. Nikmati pasir putih yang lembut di bawah kaki Anda dan deburan ombak yang menenangkan. Jalan santai di pantai adalah cara yang ideal untuk merelaksasikan pikiran dan tubuh Anda.'
+        },
+        mancing: {
+            image: 'wisata-alam/pantai-1.jpg',
+            title: 'Mancing',
+            description: ' Spot Memancing Terbaik: Pantai Mbah Sirah menawarkan spot-spot memancing yang strategis dan kaya ikan. Dari bibir pantai yang landai hingga area perairan yang lebih dalam, Anda akan menemukan berbagai jenis ikan yang siap menyambut umpan Anda. Nikmati kesempatan untuk menangkap ikan-ikan lokal yang lezat dan berharga sebagai hasil dari usaha Anda.'
+        },
+        spotfoto: {
+            image: 'wisata-alam/pantai-1.jpg',
+            title: 'Spot Foto',
+            description: 'Pemandangan Laut yang Menakjubkan: Pantai Mbah Sirah menawarkan pemandangan laut yang spektakuler, ideal untuk foto-foto yang memukau. Dari cakrawala yang membentang luas hingga warna-warni matahari terbenam, setiap sudut pantai ini memberikan latar belakang yang sempurna untuk foto-foto indah. Ambil gambar saat matahari terbenam atau matahari terbit untuk hasil yang menakjubkan dan penuh warna.'
+        }
+    };
+
+    const icons = document.querySelectorAll('.icon');
+    const activityImage = document.getElementById('activity-image-pantaimbahsirah');
+    const activityTitle = document.getElementById('activity-title-pantaimbahsirah');
+    const activityDescription = document.getElementById('activity-description-pantaimbahsirah');
+    let activeIcon = null;
+
+    function updateActivity(activity) {
+        activityImage.src = activitiesmbahsirah[activity].image;
+        activityTitle.textContent = activitiesmbahsirah[activity].title;
+        activityDescription.textContent = activitiesmbahsirah[activity].description;
+
+       
+        icons.forEach(i => {
+            i.classList.remove('active-pantaimbahsirah');
+        });
+        activeIcon = document.querySelector(`[data-activity-pantaimbahsirah="${activity}"]`);
+        activeIcon.classList.add('active-pantaimbahsirah');
+    }
+
+    icons.forEach(icon => {
+        icon.addEventListener('click', () => {
+            const activity = icon.getAttribute('data-activity-pantaimbahsirah');  
+            updateActivity(activity);
+        });
+    });
+
+    // Set default activity
+    const defaultActivity = 'berenang';
+    updateActivity(defaultActivity);
+});
+
+
+
+/*AKTIVITAS*/
+document.addEventListener('DOMContentLoaded', () => {
+    const activitieskalibuntung = {
+        berenang: {
+            image: 'wisata-alam/pantai-1.jpg',
+            title: 'Berenang',
+            description: 'Nikmati Kegembiraan Berenang: Pantai Kalibuntung menawarkan air laut yang jernih dan menyegarkan, sempurna untuk berenang dan bermain air bersama keluarga dan teman-teman. Rasakan ketenangan dan kebahagiaan saat Anda berenang di ombak yang tenang dan bersih.'
+        },
+        ayunan: {
+            image: 'wisata-alam/pantai-1.jpg',
+            title: 'Ayunan',
+            description: 'Ayunan di Tepi Pantai: Pantai Kalibuntung menawarkan pengalaman unik bermain ayunan dengan pemandangan laut yang memukau. Nikmati hembusan angin laut yang menyegarkan sambil berayun-ayun di bawah pohon kelapa yang rindang. Ini adalah cara sempurna untuk bersantai dan menikmati alam.'
+        },
+        jalan: {
+            image: 'wisata-alam/pantai-1.jpg',
+            title: 'Jalan Santai',
+            description: 'Jalan Santai di Tepi Pantai: Pantai Kalibuntung menawarkan pengalaman yang tenang dan menyegarkan dengan berjalan kaki di sepanjang garis pantai. Nikmati pasir putih yang lembut di bawah kaki Anda dan deburan ombak yang menenangkan. Jalan santai di pantai adalah cara yang ideal untuk merelaksasikan pikiran dan tubuh Anda.'
+        },
+        sepeda: {
+            image: 'wisata-alam/pantai-1.jpg',
+            title: 'Sepeda',
+            description: 'Jalan Santai di Tepi Pantai: Pantai Kalibuntung menawarkan pengalaman yang tenang dan menyegarkan dengan berjalan kaki di sepanjang garis pantai. Nikmati pasir putih yang lembut di bawah kaki Anda dan deburan ombak yang menenangkan. Jalan santai di pantai adalah cara yang ideal untuk merelaksasikan pikiran dan tubuh Anda.'
+        },
+        mancing: {
+            image: 'wisata-alam/pantai-1.jpg',
+            title: 'Mancing',
+            description: ' Spot Memancing Terbaik: Pantai Kalibuntung menawarkan spot-spot memancing yang strategis dan kaya ikan. Dari bibir pantai yang landai hingga area perairan yang lebih dalam, Anda akan menemukan berbagai jenis ikan yang siap menyambut umpan Anda. Nikmati kesempatan untuk menangkap ikan-ikan lokal yang lezat dan berharga sebagai hasil dari usaha Anda.'
+        },
+        spotfoto: {
+            image: 'wisata-alam/pantai-1.jpg',
+            title: 'Spot Foto',
+            description: 'Pemandangan Laut yang Menakjubkan: Pantai Kalibuntung menawarkan pemandangan laut yang spektakuler, ideal untuk foto-foto yang memukau. Dari cakrawala yang membentang luas hingga warna-warni matahari terbenam, setiap sudut pantai ini memberikan latar belakang yang sempurna untuk foto-foto indah. Ambil gambar saat matahari terbenam atau matahari terbit untuk hasil yang menakjubkan dan penuh warna.'
+        }
+    };
+
+    const icons = document.querySelectorAll('.icon');
+    const activityImage = document.getElementById('activity-image-pantaikalibuntung');
+    const activityTitle = document.getElementById('activity-title-pantaikalibuntung');
+    const activityDescription = document.getElementById('activity-description-pantaikalibuntung');
+    let activeIcon = null;
+
+    function updateActivity(activity) {
+        activityImage.src = activitieskalibuntung[activity].image;
+        activityTitle.textContent = activitieskalibuntung[activity].title;
+        activityDescription.textContent = activitieskalibuntung[activity].description;
+
+       
+        icons.forEach(i => {
+            i.classList.remove('active-pantaikalibuntung');
+        });
+        activeIcon = document.querySelector(`[data-activity-pantaikalibuntung="${activity}"]`);
+        activeIcon.classList.add('active-pantaikalibuntung');
+    }
+
+    icons.forEach(icon => {
+        icon.addEventListener('click', () => {
+            const activity = icon.getAttribute('data-activity-pantaikalibuntung');  
+            updateActivity(activity);
+        });
+    });
+
+    // Set default activity
+    const defaultActivity = 'berenang';
+    updateActivity(defaultActivity);
+});
+
+
+
+
+/*AKTIVITAS*/
+document.addEventListener('DOMContentLoaded', () => {
+    const activitiesmororejo = {
+        berenang: {
+            image: 'wisata-alam/pantai-1.jpg',
+            title: 'Berenang',
+            description: 'Nikmati Kegembiraan Berenang: Pantai Mororejo menawarkan air laut yang jernih dan menyegarkan, sempurna untuk berenang dan bermain air bersama keluarga dan teman-teman. Rasakan ketenangan dan kebahagiaan saat Anda berenang di ombak yang tenang dan bersih.'
+        },
+        ayunan: {
+            image: 'wisata-alam/pantai-1.jpg',
+            title: 'Ayunan',
+            description: 'Ayunan di Tepi Pantai: Pantai Mororejo menawarkan pengalaman unik bermain ayunan dengan pemandangan laut yang memukau. Nikmati hembusan angin laut yang menyegarkan sambil berayun-ayun di bawah pohon kelapa yang rindang. Ini adalah cara sempurna untuk bersantai dan menikmati alam.'
+        },
+        jalan: {
+            image: 'wisata-alam/pantai-1.jpg',
+            title: 'Jalan Santai',
+            description: 'Jalan Santai di Tepi Pantai: Pantai Mororejo menawarkan pengalaman yang tenang dan menyegarkan dengan berjalan kaki di sepanjang garis pantai. Nikmati pasir putih yang lembut di bawah kaki Anda dan deburan ombak yang menenangkan. Jalan santai di pantai adalah cara yang ideal untuk merelaksasikan pikiran dan tubuh Anda.'
+        },
+        sepeda: {
+            image: 'wisata-alam/pantai-1.jpg',
+            title: 'Sepeda',
+            description: 'Jalan Santai di Tepi Pantai: Pantai Mororejo menawarkan pengalaman yang tenang dan menyegarkan dengan berjalan kaki di sepanjang garis pantai. Nikmati pasir putih yang lembut di bawah kaki Anda dan deburan ombak yang menenangkan. Jalan santai di pantai adalah cara yang ideal untuk merelaksasikan pikiran dan tubuh Anda.'
+        },
+        mancing: {
+            image: 'wisata-alam/pantai-1.jpg',
+            title: 'Mancing',
+            description: ' Spot Memancing Terbaik: Pantai Mororejo menawarkan spot-spot memancing yang strategis dan kaya ikan. Dari bibir pantai yang landai hingga area perairan yang lebih dalam, Anda akan menemukan berbagai jenis ikan yang siap menyambut umpan Anda. Nikmati kesempatan untuk menangkap ikan-ikan lokal yang lezat dan berharga sebagai hasil dari usaha Anda.'
+        },
+        spotfoto: {
+            image: 'wisata-alam/pantai-1.jpg',
+            title: 'Spot Foto',
+            description: 'Pemandangan Laut yang Menakjubkan: Pantai Mororejo menawarkan pemandangan laut yang spektakuler, ideal untuk foto-foto yang memukau. Dari cakrawala yang membentang luas hingga warna-warni matahari terbenam, setiap sudut pantai ini memberikan latar belakang yang sempurna untuk foto-foto indah. Ambil gambar saat matahari terbenam atau matahari terbit untuk hasil yang menakjubkan dan penuh warna.'
+        }
+    };
+
+    const icons = document.querySelectorAll('.icon');
+    const activityImage = document.getElementById('activity-image-pantaimororejo');
+    const activityTitle = document.getElementById('activity-title-pantaimororejo');
+    const activityDescription = document.getElementById('activity-description-pantaimororejo');
+    let activeIcon = null;
+
+    function updateActivity(activity) {
+        activityImage.src = activitiesmororejo[activity].image;
+        activityTitle.textContent = activitiesmororejo[activity].title;
+        activityDescription.textContent = activitiesmororejo[activity].description;
+
+       
+        icons.forEach(i => {
+            i.classList.remove('active-pantaimororejo');
+        });
+        activeIcon = document.querySelector(`[data-activity-pantaimororejo="${activity}"]`);
+        activeIcon.classList.add('active-pantaimororejo');
+    }
+
+    icons.forEach(icon => {
+        icon.addEventListener('click', () => {
+            const activity = icon.getAttribute('data-activity-pantaimororejo');  
+            updateActivity(activity);
+        });
+    });
+
+    // Set default activity
+    const defaultActivity = 'berenang';
+    updateActivity(defaultActivity);
 });
